001415674086619f337954de85146f71c97c5070
Jan Kožnárek <xryx1111@gmail.com>
2025-08-13 02:20:10 +0200

    Initial Commit

---
+++ audioryTests/audioryTests.swift
@@ -0,0 +1,17 @@
+//
+//  audioryTests.swift
+//  audioryTests
+//
+//  Created by Jan Kožnárek on 13.08.2025.
+//
+
+import Testing
+@testable import audiory
+
+struct audioryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
